3bd0f54416fed5971ccb2042c80cff24bbd778bc
lsamorim <lsamorim.it@gmail.com>
2018-02-03 17:32:27 -0200

    Implement one signal

--- src/XFOneSignal/XFOneSignal/App.xaml.cs
+++ src/XFOneSignal/XFOneSignal/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Com.OneSignal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,9 @@ namespace XFOneSignal
 			InitializeComponent();
 
 			MainPage = new XFOneSignal.MainPage();
-		}
+            OneSignal.Current.StartInit("d777c476-4bc8-4a3b-8ba7-2c1b484a32cd")
+                 .EndInit();
+        }
 
 		protected override void OnStart ()
 		{
